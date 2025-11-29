387b9987a92003a4d7f3e4f4acbe34f8769533a0
Hakaisha1 <ixforce4@gmail.com>
2025-11-29 13:45:41 +0700

    feat: Update admin dashboard to display total books and borrowers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,4 +11,5 @@ class AdminController extends Controller
     {
         return view('admin-page.admin-dashboard');
     }
+    
 }
--- resources/views/admin-page/content-dashboard-admin.blade.php
+++ resources/views/admin-page/content-dashboard-admin.blade.php
@@ -1,7 +1,17 @@
 <!-- board total buku dan peminjam -->
- <div>
-
- </div>
+ <div class= "bg-white rounded-[20px] p-6 transition transform duration-300 ease-in-out">
+    <h2 class="text-[#0077B6] text-3xl font-bold font-['Roboto'] mb-6">Total Buku dan Peminjam</h2>
+    <div class="flex gap-8">
+        <div class="bg-[#caf0f8] rounded-lg p-4 flex-1 hover:bg-[#90e0ef] transition-colors">
+            <h3 class="text-[#0077B6] text-xl font-bold font-['Roboto']">Total Buku</h3>
+            <p class="text-[#49454f] text-2xl font-['Roboto'] mt-2">{{ $totalBooks }}</p>
+        </div>
+        <div class="bg-[#caf0f8] rounded-lg p-4 flex-1 hover:bg-[#90e0ef] transition-colors">
+            <h3 class="text-[#0077B6] text-xl font-bold font-['Roboto']">Total Peminjam</h3>
+            <p class="text-[#49454f] text-2xl font-['Roboto'] mt-2">{{ $totalBorrowers }}</p>
+        </div>
+    </div>
+</div>
 
  <!-- board jumlah peminjam -->
   <div>
